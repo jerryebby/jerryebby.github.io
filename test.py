c7db4f4e2ca5330f539e0df513b2d3ed704f92d2
1anmiao <jexywa333@gmail.com>
2018-09-16 19:00:28 +0800

    modify teat.py

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
 # coding=utf-8
 import MySQLdb
 
-SQLdb_id = 'root'
-SQLdb_pwd = 'aj851226'
-db = MySQLdb.connect('localhost', SQLdb_id, SQLdb_pwd, 'clothespricecompare', charset='utf8' )
+SQLdb_id = 'python_crawl'
+SQLdb_pwd = 'U8teriWxe0ozp0rf'
+db = MySQLdb.connect('localhost', SQLdb_id, SQLdb_pwd, 'CLOTHES_PRICE_COMPARE', charset='utf8' )
 cursor = db.cursor()
 
 gender = 'male'
@@ -15,7 +15,7 @@ sale_price = 3
 link = 'https://www.lativ.com.tw/Detail/34930041'
 photo = 'https://s1.lativ.com.tw/i/34930/34930_D_21_n.jpg'
 
-sql = ("INSERT INTO `product`(`gender`, `category`, `brand`, `product_name`, `original_price`, `sale_price`, `link`, `photo`) VALUES ('%s', '%s', '%s', '%s', %f, %f, '%s', '%s')" % (gender,category,brand,product_name,original_price,sale_price,link,photo))
+sql = ("INSERT INTO `PRODUCT`(`gender`, `category`, `brand`, `product_name`, `original_price`, `sale_price`, `link`, `photo`) VALUES ('%s', '%s', '%s', '%s', %f, %f, '%s', '%s')" % (gender,category,brand,product_name,original_price,sale_price,link,photo))
 cursor.execute(sql)
 db.commit()
 db.close()
