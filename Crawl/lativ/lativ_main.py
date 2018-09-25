d607c9a7fb50d6d5e88f49867abe17591de513f1
1anmiao <jexywa333@gmail.com>
2018-09-25 13:42:34 +0800

    update lativ_main.py

--- Crawl/lativ/lativ_main.py
+++ Crawl/lativ/lativ_main.py
@@ -12,10 +12,12 @@ def lativ_hrefSearch(url):
 	soup = BeautifulSoup(res.text, 'lxml')
 	tmp = soup.select('ul[class="sale_category"] a')
 	for t in tmp:
-		lativ_categorySearch(t.get('href'))
+		lativ_categorySearch(t.get('href'), url)
 
-def lativ_categorySearch(url):
-	product_data = namedtuple('product_data',['brand','product_name','original_price','sale_price','link','photo'])
+def lativ_categorySearch(url, gender):
+	if gender == 'BABY':
+		gender = 'KIDS'
+	product_data = namedtuple('product_data',['gender','brand','product_name','original_price','sale_price','link','photo'])
 	driver = webdriver.PhantomJS(executable_path='/home/clothespricecompare/phantomjs-2.1.1-linux-x86_64/bin/phantomjs')
 	wholeUrl = 'https://www.lativ.com.tw/' + url
 	driver.get(wholeUrl)
@@ -47,7 +49,7 @@ def lativ_categorySearch(url):
 			else:
 				original_price = int(t.find('span', class_='currency symbol').text)
 				sale_price = -1
-			data_list.append(product_data('lativ', product_name, original_price, sale_price, link, photo))
+			data_list.append(product_data(gender, 'lativ', product_name, original_price, sale_price, link, photo))
 	insertToDB(data_list)
 
 def insertToDB(data_list):
@@ -56,9 +58,9 @@ def insertToDB(data_list):
 	db = MySQLdb.connect('localhost', SQLdb_id, SQLdb_pwd, 'clothespricecompare', charset='utf8' )
 	cursor = db.cursor()
 	for index in data_list:
-		sql = ("INSERT INTO `PRODUCT`(`brand`, `product_name`, `original_price`, `sale_price`, `link`, `photo`)\
-		 VALUES ('%s', '%s', %f, %f, '%s', '%s')" % \
-		(index.brand, index.product_name, index.original_price, index.sale_price, index.link, index.photo))
+		sql = ("INSERT INTO `PRODUCT`(`gender`, `brand`, `product_name`, `original_price`, `sale_price`, `link`, `photo`)\
+		 VALUES ('%s', '%s', '%s', %f, %f, '%s', '%s')" % \
+		(index.gender, index.brand, index.product_name, index.original_price, index.sale_price, index.link, index.photo))
 		cursor.execute(sql)
 	db.commit()
 	db.close()
