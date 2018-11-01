e0ea735343e6e6c6d8c8c5049069a41c6dc1d2fa
1anmiao <jexywa333@gmail.com>
2018-11-01 22:19:42 +0800

    update GU crawl

--- Crawl/GU/GU_main.py
+++ Crawl/GU/GU_main.py
@@ -1,6 +1,5 @@
 # coding=utf-8
 import requests
-import re
 from bs4 import BeautifulSoup
 import MySQLdb
 from collections import namedtuple
@@ -17,12 +16,13 @@ def GU_categorySearch(url):
     for i in items:
         tmp = i.select('dt.name > a')
         product_name = tmp[0].text.encode('utf-8')
+        category = getCategory(url, product_name)
         link = tmp[0].get('href')
         tmp = i.select('dd.thumb > a > img')
         photo = tmp[0].get('src')
         tmp = i.select('dd.price')
         original_price = stripPrice(tmp[0].text)
-        data_list.append(product_data(gender, '', 'GU', product_name, \
+        data_list.append(product_data(gender, category, 'GU', product_name, \
 		original_price, -1, link, photo))
     insertToDB(data_list)
 
@@ -43,6 +43,86 @@ def getGender(url):
     else:
         return ''
 
+"""
+上衣 UPPER
+	短袖 SHORT_SLEEVES
+	長袖 LONG_SLEEVES
+	POLO衫 POLO
+	襯衫 SHIRT
+	外套  OUTERWEAR
+下身 BOTTOM
+	短褲 SHORTS
+	長褲 TROUSERS
+	牛仔褲 JEANS
+	裙裝 SKIRT（WOMEN）
+其他 OTHER
+	運動 SPORTS
+	配件 ACCESSORIES
+	內衣 UNDERWEAR
+	內褲 UNDERPANTS
+"""
+
+def getCategory(url, product_name):
+    category = namedtuple('category',['primary','minor'])
+    if '/coat/' in url or '/jacket/' in url or '/cardigan/' in url or '/parka/' in url \
+    or '/uvcut/' in url or '/windcoat/' in url or '/girlsouter/' in url or '/boysouter/' in url:
+        return category('UPPER', 'OUTERWEAR')
+    elif '/sweat/' in url or '/knit/' in url or '/tunic/' in url or '/designtops/' in url \
+    or '/printT/' in url or '/poloshirt/' in url or '/innerT/' in url or '/girlstops/' in url \
+    or '/boystops/' in url:
+        return getCategoryByPName(product_name, 'UPPER', 'LONG_SLEEVES')
+    elif '/longsleevetshirts/' in url:
+        return category('UPPER', 'LONG_SLEEVES')
+    elif '/shirts/' in url:
+        return category('UPPER', 'SHIRT')
+    elif '/onepiece/' in url:
+        return getCategoryByPName(product_name, 'BOTTOM', 'SKIRT')
+    elif '/widepants/' in url or '/pants/' in url or '/leggingspants/' in url \
+    or '/croppedpants/' in url or '/suitpants/' in url or '/girlsbottoms/' in url \
+    or '/boysbottoms/' in url:
+        return category('BOTTOM', 'TROUSERS')
+    elif '/skirt/' in url:
+        return category('BOTTOM', 'SKIRT')
+    elif '/jeans/' in url:
+        return category('BOTTOM', 'JEANS')
+    elif '/relaxedpants/' in url:
+        return getCategoryByPName(product_name, 'BOTTOM', 'TROUSERS')
+    elif '/shortpants/' in url:
+        return category('BOTTOM', 'SHORTS')
+    elif '/brafeel/' in url or '/underwear/' in url:
+        return category('OTHER', 'UNDERWEAR')
+    elif '/guwarm/' in url or '/gudry/' in url or '/undershirts/' in url:
+        return getCategoryByPName(product_name, 'UPPER', '')
+    elif '/boxers/' in url:
+        return category('OTHER', 'UNDERPANTS')
+    elif '/roomwear/' in url:
+        return getCategoryByPName(product_name, 'OTHER', 'ACCESSORIES')
+    elif '/socks/' in url or '/shoes/' in url or '/bag/' in url or '/pierce/' in url \
+    or '/accessory/' in url or '/hairaccessory/' in url or '/hat/' in url or '/belt/' in url \
+    or '/scarf/' in url or '/goods/' in url:
+        return category('OTHER', 'ACCESSORIES')
+
+def getCategoryByPName(product_name, p, m):
+    category = namedtuple('category',['primary','minor'])
+    if u'外套' in product_name:
+		return category('UPPER', 'OUTERWEAR')
+    elif u'裙' in product_name or u'洋裝' in product_name:
+		return category('BOTTOM', 'SKIRT')
+    elif u'短褲' in product_name or u'1分' in product_name or u'3分' in product_name:
+		return category('BOTTOM', 'SHORTS')
+    elif u'褲' in product_name:
+		return category('BOTTOM', 'TROUSERS')
+    elif u'襯衫' in product_name:
+		return category('UPPER', 'SHIRT')
+    elif u'長袖' in product_name or u'5分袖' in product_name or u'7分袖' in product_name \
+    or u'8分袖' in product_name:
+        return category('UPPER', 'LONG_SLEEVES')
+    elif u'背心' in product_name or u'短袖' in product_name or u'無袖' in product_name or \
+    u'細肩帶' in product_name:
+        return category('UPPER', 'SHORT_SLEEVES')
+    else:
+		return category(p, m)
+
 def insertToDB(data_list):
     SQLdb_id = 'python_crawl'
     SQLdb_pwd = 'U8teriWxe0ozp0rf'
@@ -50,9 +130,9 @@ def insertToDB(data_list):
     cursor = db.cursor()
     for index in data_list:
         sql = ("INSERT INTO `PRODUCT`(`gender`, `primary_category`, `minor_category`, `brand`, \
-        `product_name`, `original_price`, `sale_price`, `link`, `photo`)\
+        `product_name`, `original_price`, `sale_price`, `link`, `photo`) \
         VALUES ('%s', '%s', '%s', '%s', '%s', %f, %f, '%s', '%s');" % \
-        (index.gender, '', '', index.brand, \
+        (index.gender, index.category.primary, index.category.minor, index.brand, \
         index.product_name, index.original_price, index.sale_price, index.link, index.photo))
         cursor.execute(sql)
         print(sql)
@@ -72,10 +152,14 @@ def cleanExpiredData():
     db.commit()
     db.close()
 
-url = 'http://www.gu-global.com/tw/'
-res = requests.get(url)
+res = requests.get('http://www.gu-global.com/tw/')
 soup = BeautifulSoup(res.text, 'lxml')
 tmp = soup.select('ul[class="sub2"] a')
+url_list = []
 for t in tmp:
-    GU_categorySearch(t.get('href'))
-cleanExpiredData()
+    url = t.get('href')
+    if url not in url_list:
+        url_list.append(url)
+        print url
+        #GU_categorySearch(t.get('href'))
+#cleanExpiredData()
